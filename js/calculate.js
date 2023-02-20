406681ad7bb361318ffdb1c6b23088123682bc53
Eduardo de Oliveira <eduardoolive133@gmail.com>
2023-02-19 23:21:58 -0300

    css - commiting css files

--- js/calculate.js
+++ js/calculate.js
@@ -14,57 +14,84 @@ const DOM = {
             healthyWeightLimit: 24.9,
             overweightLimit: 29.9
         }
-        
-        console.log(BMIData)
 
-        const resSection = document.querySelector('#result')
         let title = document.createElement('h2')
         let content = document.createElement('p')
 
         if (BMIData <=  range.underweightLimit) {
             title.innerHTML = 
             `
-            <h2>Underweight</h2>
+            <hr>
+            <div class="result-title">
+                <h2>Underweight</h2>
+                <div class="bmi-result">${BMIData}</div>
+            </div>
             `
             
             content.innerHTML = 
             `
             <p>If your BMI is less than 18.5, it falls within the underweight range.</p>
             `
+            DOM.appendChild(title, content)
+            document.querySelector('div.bmi-result').classList.add('danger')
         }
-        else if (BMIData <= range.healthyWeightLimit) {
+        else if (BMIData <= range.healthyWeightLimit) {     
             title.innerHTML = 
             `
-            <h2>Healthy Weight</h2>
+            <hr>
+            <div class="result-title">
+                <h2>Healthy Weight</h2>
+                <div class="bmi-result">${BMIData}</div>
+            </div>
             `
 
             content.innerHTML = 
             `
             <p>If your BMI is 18.5 to 24.9, it falls within the Healthy Weight range.</p>
             `
+            DOM.appendChild(title, content)
+            document.querySelector('div.bmi-result').classList.add('healthy')
         }
         else if (BMIData <= range.overweightLimit) {
             title.innerHTML = 
             `
-            <h2>Overweight</h2>
+            <hr>
+            <div class="result-title">
+                <h2>Overweight</h2>
+                <div class="bmi-result">${BMIData}</div>
+            </div>
             `
             
             content.innerHTML = 
             `
             <p>If your BMI is 25.0 to 29.9, it falls within the overweight range.</p>
             `
+            DOM.appendChild(title, content)
+            document.querySelector('div.bmi-result').classList.add('caution')
         }
         else {
             title.innerHTML = 
             `
-            <h2>Obese</h2>
+            <hr>
+            <div class="result-title">
+                <h2>Obese</h2>
+                <div class="bmi-result">${BMIData}</div>
+            </div>
             `
             
             content.innerHTML = 
             `
             <p>If your BMI is 30.0 or higher, it falls within the obese range.</p>
             `
+            DOM.appendChild(title, content)
+            document.querySelector('div.bmi-result').classList.add('danger')
         }  
+        
+    },
+
+    appendChild(title, content) {
+        const resSection = document.querySelector('#result')
+
         resSection.appendChild(title)
         resSection.appendChild(content)
     },
@@ -79,9 +106,9 @@ const CalculateBMI = {
     calculate() {
         const amount = GetData.getWeightAndHeight()
         const weight = amount[0]
-        const height = amount[1]
-        let BMIResult = weight / height ** 2
-        Utilities.formatAmount(BMIResult)
+        let height = amount[1]
+        height = height / 100
+        let BMIResult = Utilities.formatAmount(weight / height ** 2)
         return BMIResult
     }
 }
@@ -100,8 +127,8 @@ const Utilities = {
         event.preventDefault()
     },
 
-    formatAmount(BMIResult) {
-        BMIResult.toFixed(2)
+    formatAmount(value) {
+        return value.toFixed(2)
     }
 }
 
